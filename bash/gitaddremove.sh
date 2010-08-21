b9c0f30e657c61348f398bbb63d9a63ebea02b5e
Adam Ramadhan <adamramadhan@live.com>
2010-08-21 17:08:44 +0700

    added more way to do this next an indonesian git alias

--- bash/gitaddremove.sh
+++ bash/gitaddremove.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 # git-addremove
+
+# add the below
+
+#[alias]
+#    addremove = !git add . && git ls-files --deleted | xargs --#no-run-if-empty git rm
+
+# at $HOME/.gitconfig
+
 git add .
-git ls-files --deleted | xargs git rm
+git ls-files --deleted | xargs --no-run-if-empty git rm
