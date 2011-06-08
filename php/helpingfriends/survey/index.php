de2cfaee7acc8461ae98daef0a63260d5433fe48
Adam Ramadhan <adamramadhan@live.com>
2011-06-08 17:21:32 +0700

    versi acakadut #2 fast epic prototypeing

--- php/helpingfriends/survey/index.php
+++ php/helpingfriends/survey/index.php
@@ -17,18 +17,28 @@ if (!empty($_GET['s'])) {
 		$user['email'] = $_POST['email'];
 		$user['tlp'] = $_POST['tlp'];
 		$user['kota'] =$_POST['kota'];
-		
+		var_dump($user);
 		# todo validasi
 		
 		# MASUKIN USER BARU AMBIL UIDNYA
-		#$uid = $db->insertwithlastid('users',$user);
+		$uid = $db->insertwithlastid('users',$user);
 
-		#
-		foreach ($_POST['answers'] as $answer) {
-			$sq[] = "('".$answer."',".$_GET['s'].")";
+		$answers = $_POST['answers'];
+
+		if (!empty($uid) && !empty($answers)) {
+			foreach ($answers as $answer) {
+				#var_dump(key($answers));
+				$sq[] = "('".$answer."',".key($answers).",".$uid.")";
+				next($answers);
+			}
+			$query = implode(',', $sq);
+			$fullquery = 'INSERT INTO answers(text,QID,UID) VALUES '.$query;
 		}
-
-
+		$status = $db->query($fullquery);
+		
+		if ($status) {
+			echo "<div id=\"statues\">Answers and User saved.</div>";
+		}
 	}
 	require '/pages/index.php';
 }
